d7b9ab7dfb74e2a6989142c362c2f1436d15f76b
Ed Hickey <erhickey@gmail.com>
2022-12-04 00:43:29 -0500

    2022 day 4

--- rust/src/y2022/day4.rs
+++ rust/src/y2022/day4.rs
@@ -1,5 +1,16 @@
-use std::fmt::Display;
+use std::{fmt::Display, collections::HashSet};
 
-pub fn solve(_input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
-    (Box::new(0), Box::new(0))
+fn parse_pair(s: &str) -> (HashSet<u32>, HashSet<u32>) {
+    let ns: [u32; 4] = s.split(|c: char| !c.is_numeric()).map(|s| s.parse::<u32>())
+        .flatten().collect::<Vec<u32>>().try_into().unwrap();
+    ((ns[0]..ns[1] + 1).collect(), (ns[2]..ns[3] + 1).collect())
+}
+
+pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
+    let pairs = input.lines().map(parse_pair);
+
+    (
+        Box::new(pairs.clone().filter(|(s1, s2)| s1.is_subset(&s2) || s2.is_subset(&s1)).count()),
+        Box::new(pairs.filter(|(s1, s2)| s1.intersection(s2).count() > 0).count())
+    )
 }
