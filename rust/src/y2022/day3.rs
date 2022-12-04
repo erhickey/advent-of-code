df96d6ffb545670a39d6925ebba39e2fee85d06b
Ed Hickey <erhickey@gmail.com>
2022-12-03 22:34:19 -0500

    2022 day 3 improvements

--- rust/src/y2022/day3.rs
+++ rust/src/y2022/day3.rs
@@ -1,20 +1,29 @@
-use std::{fmt::Display, collections::HashSet};
+use std::{fmt::Display, collections::HashSet, hash::Hash};
 
-fn split_sack(s: &str) -> Vec<&str> {
-    let (s1, s2) = s.split_at(s.len() / 2);
-    vec![s1, s2]
+fn intersections<T: Eq + Hash>(vs: Vec<Vec<T>>) -> Vec<T> {
+    vs.into_iter()
+        .map(|v| v.into_iter().collect::<HashSet<T>>())
+        .fold(HashSet::new(), |mut acc, hs| {
+            match acc.len() {
+                0 => hs,
+                _ => { acc.retain(|e| hs.contains(e)); acc }
+            }
+        })
+        .drain()
+        .collect()
 }
 
-fn common_item(ss: Vec<&str>) -> char {
-    ss.iter().fold(HashSet::new(), |mut acc, s| {
-        if acc.len() > 0 {
-            let s_set: HashSet<char> = s.chars().collect();
-            acc.retain(|c| s_set.contains(c));
-            acc
-        } else {
-            s.chars().collect()
-        }
-    }).drain().collect::<Vec<char>>()[0]
+fn first<T>(v: Vec<T>) -> Option<T> {
+    v.into_iter().nth(0)
+}
+
+fn chunks<T: Clone>(v: Vec<T>, size: usize) -> Vec<Vec<T>> {
+    v.chunks(size).map(|e| e.into()).collect()
+}
+
+fn bisect(s: &str) -> Vec<Vec<char>> {
+    let (s1, s2) = s.split_at(s.len() / 2);
+    vec![s1.chars().collect(), s2.chars().collect()]
 }
 
 fn item_priority(c: char) -> u32 {
@@ -27,9 +36,10 @@ fn item_priority(c: char) -> u32 {
 pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
     let sacks = input.lines();
     (
-        Box::new(sacks.clone().map(split_sack).map(common_item).map(item_priority).sum::<u32>()),
+        Box::new(sacks.clone().map(bisect).map(intersections).map(first).flatten().map(item_priority).sum::<u32>()),
         Box::new(
-            sacks.collect::<Vec<&str>>().chunks(3).collect::<Vec<&[&str]>>().iter().map(|a| a.to_vec())
-                .collect::<Vec<Vec<&str>>>().into_iter().map(common_item).map(item_priority).sum::<u32>())
+            chunks(sacks.map(|s| s.chars().collect()).collect(), 3)
+                .into_iter().map(intersections).map(first).flatten().map(item_priority).sum::<u32>()
+        )
     )
 }
