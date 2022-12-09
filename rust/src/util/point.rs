ce73975579fc1b6569be3efbfd7d6bf6d01f2295
Ed Hickey <erhickey@gmail.com>
2022-12-09 18:44:23 -0500

    2022 day 9

---
+++ rust/src/util/point.rs
@@ -0,0 +1,94 @@
+use std::collections::HashSet;
+
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
+pub struct Point {
+    pub x: i32,
+    pub y: i32
+}
+
+impl Point {
+    pub fn move_up(&self, n: i32) -> Point {
+        Point { x: self.x, y: self.y + n }
+    }
+
+    pub fn move_down(&self, n: i32) -> Point {
+        Point { x: self.x, y: self.y - n }
+    }
+
+    pub fn move_left(&self, n: i32) -> Point {
+        Point { x: self.x - n, y: self.y }
+    }
+
+    pub fn move_right(&self, n: i32) -> Point {
+        Point { x: self.x + n, y: self.y }
+    }
+
+    #[allow(dead_code)]
+    pub fn adjacent_points(&self) -> HashSet<Point> {
+        [
+            Point { x: self.x - 1, y: self.y + 1 },
+            Point { x: self.x - 1, y: self.y },
+            Point { x: self.x - 1, y: self.y - 1 },
+            Point { x: self.x, y: self.y - 1 },
+            Point { x: self.x, y: self.y + 1 },
+            Point { x: self.x + 1, y: self.y + 1 },
+            Point { x: self.x + 1, y: self.y },
+            Point { x: self.x + 1, y: self.y - 1 }
+        ].into_iter().collect()
+    }
+
+    #[allow(dead_code)]
+    pub fn is_adjacent(&self, point: &Point) -> bool {
+        self.adjacent_points().contains(point)
+    }
+
+    #[allow(dead_code)]
+    pub fn is_adjacent_or_equal(&self, point: &Point) -> bool {
+        self.is_adjacent(point) || self == point
+    }
+
+    pub fn x_distance(&self, point: &Point) -> i32 {
+        (self.x - point.x).abs()
+    }
+
+    pub fn y_distance(&self, point: &Point) -> i32 {
+        (self.y - point.y).abs()
+    }
+
+    pub fn is_left_of(&self, point: &Point) -> bool {
+        self.x < point.x
+    }
+
+    pub fn is_under(&self, point: &Point) -> bool {
+        self.y < point.y
+    }
+}
+
+#[allow(dead_code)]
+pub fn pretty_print_grid(ps: &HashSet<Point>) -> String {
+    let xs: Vec<i32> = ps.clone().drain().into_iter().map(|p| p.x).collect();
+    let ys: Vec<i32> = ps.clone().drain().into_iter().map(|p| p.y).collect();
+    let min_x = xs.clone().into_iter().min().unwrap();
+    let max_x = xs.into_iter().max().unwrap();
+    let min_y = ys.clone().into_iter().min().unwrap();
+    let max_y = ys.into_iter().max().unwrap();
+
+    let mut ret: Vec<char> = (min_y..max_y + 1)
+        .flat_map(|y| {
+            let mut vs: Vec<char> = Vec::new();
+            vs.push('\n');
+            vs.extend(y.to_string().chars().rev());
+            vs.push(' ');
+            vs.extend((min_x..max_x + 1)
+                .map(|x| if ps.contains(&Point { x, y }) { '#' } else { ' ' })
+                .rev()
+                .collect::<Vec<char>>());
+            vs
+        })
+        .rev()
+        .collect();
+    ret.extend(min_x.to_string().chars());
+    ret.extend(" ".repeat((max_x - min_x).abs() as usize).chars());
+    ret.extend(max_x.to_string().chars());
+    ret.iter().collect()
+}
--- rust/src/y2022/day9.rs
+++ rust/src/y2022/day9.rs
@@ -1,5 +1,108 @@
-use std::fmt::Display;
+use std::{fmt::Display, collections::HashSet};
 
-pub fn solve(_input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
-    (Box::new(0), Box::new(0))
+use crate::util::point::Point;
+
+#[derive(Clone)]
+enum Direction {
+    U, D, L, R
+}
+
+impl Direction {
+    fn from(c: char) -> Direction {
+        match c {
+            'U' => Direction::U,
+            'D' => Direction::D,
+            'L' => Direction::L,
+            'R' => Direction::R,
+            _ => panic!()
+        }
+    }
+}
+
+#[derive(Clone)]
+struct State {
+    head: Point,
+    tail: Option<Box<State>>
+}
+
+fn parse_move(s: &str) -> Vec<Direction> {
+    let [d, c]: [&str; 2] = s.split(' ').collect::<Vec<&str>>().try_into().unwrap();
+    let dir = d.chars().next().unwrap();
+    (0..c.parse().unwrap()).into_iter().map(|_| Direction::from(dir)).collect()
+}
+
+fn update_tail(state: Option<Box<State>>, leader: Point) -> Option<Box<State>> {
+    if state.is_none() {
+        return None
+    }
+
+    let old_tail = state.unwrap();
+    let current_pos = &old_tail.head;
+
+    let head = if
+        leader.x != current_pos.x
+        && leader.y != current_pos.y
+        && (leader.x_distance(current_pos) > 1 || leader.y_distance(current_pos) > 1)
+    {
+        Point {
+            x: if leader.is_left_of(current_pos) { current_pos.x - 1 } else { current_pos.x + 1 },
+            y: if leader.is_under(current_pos) { current_pos.y - 1 } else { current_pos.y + 1 }
+        }
+    } else if leader.x_distance(current_pos) > 1 {
+        Point {
+            x: if leader.is_left_of(current_pos) { current_pos.x - 1 } else { current_pos.x + 1 },
+            y: leader.y
+        }
+    } else if leader.y_distance(current_pos) > 1 {
+        Point {
+            x: leader.x,
+            y: if leader.is_under(current_pos) { current_pos.y - 1 } else { current_pos.y + 1 }
+        }
+    } else {
+        old_tail.head
+    };
+
+    Some(Box::new(State { head: head.clone(), tail: update_tail(old_tail.tail, head) }))
+}
+
+fn do_move(state: &State, dir: &Direction) -> State {
+    let head = match dir {
+        Direction::U => state.head.move_up(1),
+        Direction::D => state.head.move_down(1),
+        Direction::L => state.head.move_left(1),
+        Direction::R => state.head.move_right(1)
+    };
+
+    State { head: head.clone(), tail: update_tail(state.tail.clone(), head) }
+}
+
+fn do_moves(moves: Vec<Direction>, tail_size: u8) -> Vec<State> {
+    let mut init: Vec<State> = Vec::new();
+    init.push(create_state(tail_size));
+
+    moves
+        .iter()
+        .fold(init, |mut acc, mv| {
+            acc.push(do_move(acc.last().unwrap(), mv));
+            acc
+        })
+}
+
+fn create_state(tail_size: u8) -> State {
+    State {
+        head: Point { x: 0, y: 0 },
+        tail: if tail_size == 0 { None } else { Some(Box::new(create_state(tail_size - 1 ))) }
+    }
+}
+
+fn get_end_position(state: State) -> Point {
+    if state.tail.is_none() { state.head } else { get_end_position(*state.tail.unwrap()) }
+}
+
+pub fn solve(input: &str) -> (Box<dyn Display>, Box<dyn Display>) {
+    let moves: Vec<Direction> = input.lines().flat_map(parse_move).collect();
+    let part1: HashSet<Point> = do_moves(moves.clone(), 1).into_iter().map(get_end_position).collect();
+    let part2: HashSet<Point> = do_moves(moves, 9).into_iter().map(get_end_position).collect();
+
+    (Box::new(part1.len()), Box::new(part2.len()))
 }
